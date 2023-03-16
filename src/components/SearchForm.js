18c41ea871cd8b6616df97cd50f17dc800c5c35f
surajverma2587 <surajverma2587@gmail.com>
2023-03-16 19:18:08 +0000

    form validation

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,3 +1,5 @@
+import * as Yup from "yup";
+import { useFormik } from "formik";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 import InputAdornment from "@mui/material/InputAdornment";
@@ -5,11 +7,34 @@ import IconButton from "@mui/material/IconButton";
 import SearchIcon from "@mui/icons-material/Search";
 
 export const SearchFrom = () => {
+  const initialValues = {
+    query: "",
+  };
+
+  const validationSchema = Yup.object({
+    query: Yup.string().required("Please enter a valid product"),
+  });
+
+  const onSubmit = ({ query }) => {
+    console.log(query);
+  };
+
+  const formik = useFormik({
+    initialValues,
+    validationSchema,
+    onSubmit,
+  });
+
   return (
-    <Box sx={{ my: 3 }} component="form">
+    <Box sx={{ my: 3 }} component="form" onSubmit={formik.handleSubmit}>
       <TextField
-        placeholder="Search amazon marketplace..."
+        name="query"
+        placeholder="Enter product name"
         fullWidth
+        value={formik.values.query}
+        error={!!formik.errors.query}
+        helperText={formik.errors.query}
+        onChange={formik.handleChange}
         InputProps={{
           endAdornment: (
             <InputAdornment position="end">
