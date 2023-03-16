b4294238f17ff4a3867cd1ff845b184897ca7a4f
surajverma2587 <surajverma2587@gmail.com>
2023-03-16 18:42:40 +0000

    responsive nav bar

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,4 +1,6 @@
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import Drawer from "@mui/material/Drawer";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
@@ -7,13 +9,53 @@ import MenuIcon from "@mui/icons-material/Menu";
 import Container from "@mui/material/Container";
 import Badge from "@mui/material/Badge";
 import Button from "@mui/material/Button";
+import Divider from "@mui/material/Divider";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemText from "@mui/material/ListItemText";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 
 export const NavigationBar = () => {
   const navigate = useNavigate();
 
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <AppBar position="static">
+      <Drawer
+        anchor="top"
+        open={isOpen}
+        onClose={() => {
+          setIsOpen(false);
+        }}
+      >
+        <List>
+          <ListItem disablePadding>
+            <ListItemButton
+              onClick={() => {
+                navigate("/");
+                setIsOpen(false);
+              }}
+            >
+              <ListItemText primary="Home" />
+            </ListItemButton>
+          </ListItem>
+
+          <Divider />
+
+          <ListItem disablePadding>
+            <ListItemButton
+              onClick={() => {
+                navigate("/wish-list");
+                setIsOpen(false);
+              }}
+            >
+              <ListItemText primary="Wish List" />
+            </ListItemButton>
+          </ListItem>
+        </List>
+      </Drawer>
       <Container maxWidth="xl">
         <Toolbar disableGutters>
           <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
@@ -23,6 +65,9 @@ export const NavigationBar = () => {
               aria-controls="menu-appbar"
               aria-haspopup="true"
               color="inherit"
+              onClick={() => {
+                setIsOpen(true);
+              }}
             >
               <MenuIcon />
             </IconButton>
